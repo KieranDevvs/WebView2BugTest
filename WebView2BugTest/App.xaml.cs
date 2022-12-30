74bf7b5ce5a7bd82157a06e3422432cf1a4d1286
Kieran Devlin <kierandevvs@gmail.com>
2022-12-30 19:55:43 +0000

    Add project files.

---
+++ WebView2BugTest/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace WebView2BugTest;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ WebView2BugTest/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WebView2BugTest;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
